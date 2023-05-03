326d87df24356ac88eea15a44e4b79215d925f58
asad2002web <asad2002web@gmail.com>
2023-05-03 10:18:38 +0600

    data get success

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ const app = express();
 const cors = require('cors')
 app.use(cors())
 
+const Dashes = require('./data/dashes.json')
 
+app.get('/', (req, res) => {
+    res.send("Server is running")
+});
+
+app.get('/dashes', (req, res) => {
+    res.send(Dashes)
+});
+
+app.get('/dashes/:id', (req, res) => {
+    const id = req.params.id;
+    const DashesId = Dashes.find(n => n.id == id)
+    res.send(DashesId)
+})
 
 
 app.listen(4000, () =>{
