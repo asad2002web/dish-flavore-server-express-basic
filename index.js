d82cfdb92753eaf6751e03825cb03947d48521fb
asad2002web <asad2002web@gmail.com>
2023-05-03 10:13:17 +0600

    json data added

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (req, res) =>{
-    res.json({name : "asadullah"})
-});
 
 app.listen(4000, () =>{
-    console.log("server running port 500")
+    console.log("server running port 4000")
 });
 
